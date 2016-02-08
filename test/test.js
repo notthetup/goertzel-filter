357f8fa333c43cd697c93913972ba0840294fc8a
Chinmay Pendharkar <notthetup@gmail.com>
2016-02-08 23:24:00 +0800

    added asm.js support

--- test/test.js
+++ test/test.js
@@ -1,23 +1,33 @@
 var gf = require('../index.js');
 
-var frequency = 400;
-
-var samples = [];
-
+var frequency = 4000;
+var offset = 0;
 var sampleRate = 44100;
-var length = 1*sampleRate;
+var length = 2*sampleRate;
 var chunkSize = 128;
 
+var samples = new Float32Array(length);
+
 for(var index = 0; index < length; index++){
-  samples[index] = Math.cos(2*Math.PI*frequency*index/length);
+  samples[index] = Math.cos(2*Math.PI*frequency*index/sampleRate);
 }
 
-gf.init(200,sampleRate);
+gf.init(frequency+offset,sampleRate, chunkSize);
 
 var numChunks = Math.floor(length/chunkSize);
+
+var result = [];
 
 for(cIndex = 0; cIndex < numChunks; cIndex++){
   var start = cIndex*chunkSize;
   var chunkSample = samples.slice(start,start+chunkSize);
-  console.log(gf.run(chunkSample));
+  result.push(gf.run(chunkSample));
 }
+
+function getAvg(values) {
+  return values.reduce(function (p, c) {
+    return p + c;
+  }) / values.length;
+}
+
+console.log(getAvg(result), Math.max.apply(null, result),Math.min.apply(null, result))
